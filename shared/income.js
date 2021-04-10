f3062f5e8ce966f7b1ae88c76992d583f1229144
Amanda Shuman <amanda.n.shuman@gmail.com>
2021-04-10 16:36:19 -0400

    Added income data

---
+++ shared/income.js
@@ -0,0 +1,38 @@
+export const INCOME = [
+    {
+        id: 0,
+        name: 'Wages'
+    },
+    {
+        id: 1,
+        name: 'Tips'
+    },
+    {
+        id: 2,
+        name: 'Interest Income'
+    },
+    {
+        id: 3,
+        name: 'Dividends'
+    },
+    {
+        id: 4,
+        name: 'Gifts'
+    },
+    {
+        id: 5,
+        name: 'Refunds/Reimbursements'
+    },
+    {
+        id: 6,
+        name: 'Financial Aid'
+    },
+    {
+        id: 7,
+        name: 'Rental Income'
+    },
+    {
+        id: 8,
+        name: 'Other'
+    }
+];
