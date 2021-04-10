097355028e90d64f73b004a3775a91e0e8f9cabf
Amanda Shuman <amanda.n.shuman@gmail.com>
2021-04-10 17:54:26 -0400

    Income tab

--- components/IncomeComponent.js
+++ components/IncomeComponent.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Card } from 'react-native-elements';
+import { ScrollView } from 'react-native-gesture-handler';
+import { INCOME } from '../shared/income';
+
+const Summary = () => {
+  return (
+    <Card>
+      <Text style={{ margin: 10 }}>
+        The first step to getting your finances in order is to figure out how much money you have to work with. Take some time to enter in the money you are receiving.
+      </Text>
+    </Card>
+  );
+}
 
 class Income extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      income: INCOME
+    }
+  }
 
   static navigationOptions = {
     title: 'Income'
@@ -9,9 +28,9 @@ class Income extends Component {
 
   render() {
     return (
-      <View>
-        <Text>Income Component</Text>
-      </View>
+      <ScrollView>
+        <Summary />
+      </ScrollView>
     );
   }
 }
