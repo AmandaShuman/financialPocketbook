6810c4486e8979ddec484a6e70b6ecf4d6056fb3
Amanda Shuman <amanda.n.shuman@gmail.com>
2021-04-10 20:08:46 -0400

    expenses added

---
+++ shared/expenses.js
@@ -0,0 +1,58 @@
+export const EXPENSES = [
+  {
+    id: 0,
+    name: 'Savings'
+  },
+  {
+    id: 1,
+    name: 'Charity / Gifts'
+  },
+  {
+    id: 2,
+    name: 'Housing'
+  },
+  {
+    id: 3,
+    name: 'Utilities'
+  },
+  {
+    id: 4,
+    name: 'Food'
+  },
+  {
+    id: 5,
+    name: 'Transportation'
+  },
+  {
+    id: 6,
+    name: 'Health'
+  },
+  {
+    id: 7,
+    name: 'Daily Living'
+  },
+  {
+    id: 8,
+    name: 'Children'
+  },
+  {
+    id: 9,
+    name: 'Obligations'
+  },
+  {
+    id: 10,
+    name: 'Business Expenses'
+  },
+  {
+    id: 11,
+    name: 'Entertainment'
+  },
+  {
+    id: 12,
+    name: 'Subscriptions'
+  },
+  {
+    id: 13,
+    name: 'Miscellaneous'
+  }
+];
