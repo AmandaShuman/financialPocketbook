240669c5e89d587ea0d448a5820bfebef44271d5
Amanda Shuman <amanda.n.shuman@gmail.com>
2021-04-11 21:07:34 -0400

    first hals of expenses list

---
+++ shared/expenseLists.js
@@ -0,0 +1,273 @@
+export const EXPENSE_LISTS = 
+[
+  {
+    id: 0,
+    expensesId: 0,
+    name: 'Emergency Fund (6-12 mo)'
+  },
+  {
+    id: 1,
+    expensesId: 0,
+    name: 'Emergency Spending'
+  },
+  {
+    id: 2,
+    expensesId: 0,
+    name: 'Retirement Fund'
+  },
+  {
+    id: 3,
+    expensesId: 0,
+    name: 'College Fund'
+  },
+  {
+    id: 4,
+    expensesId: 0,
+    name: 'Investments'
+  },
+  {
+    id: 5,
+    expensesId: 0,
+    name: 'Taxes'
+  },
+  {
+    id: 6,
+    expensesId: 0,
+    name: 'Down Payment'
+  },
+  {
+    id: 7,
+    expensesId: 0,
+    name: 'Home Repairs'
+  },
+  {
+    id: 8,
+    expensesId: 0,
+    name: 'Car Replacement'
+  },
+  {
+    id: 9,
+    expensesId: 0,
+    name: 'Car Repairs'
+  },
+  {
+    id: 10,
+    expensesId: 0,
+    name: 'Furniture Replacement'
+  },
+  {
+    id: 11,
+    expensesId: 0,
+    name: 'Vacation'
+  },
+  {
+    id: 12,
+    expensesId: 0,
+    name: 'Other Savings'
+  },
+  {
+    id: 13,
+    expensesId: 1,
+    name: 'Tithing'
+  },
+  {
+    id: 14,
+    expensesId: 1,
+    name: 'Charitable Donations'
+  },
+  {
+    id: 15,
+    expensesId: 1,
+    name: 'Religious Donations'
+  },
+  {
+    id: 16,
+    expensesId: 1,
+    name: 'Gifts'
+  },
+  {
+    id: 17,
+    expensesId: 1,
+    name: 'Christmas'
+  },
+  {
+    id: 18,
+    expensesId: 1,
+    name: 'Other Charity'
+  },
+  {
+    id: 19,
+    expensesId: 2,
+    name: 'Mortage/Rent'
+  },
+  {
+    id: 20,
+    expensesId: 2,
+    name: 'Home/Rental Insurance'
+  },
+  {
+    id: 21,
+    expensesId: 2,
+    name: 'Real Estate Taxes'
+  },
+  {
+    id: 22,
+    expensesId: 2,
+    name: 'Furnishings/Appliances'
+  },
+  {
+    id: 23,
+    expensesId: 2,
+    name: 'Lawn/Garden'
+  },
+  {
+    id: 24,
+    expensesId: 2,
+    name: 'Maintenance/Supplies'
+  },
+  {
+    id: 25,
+    expensesId: 2,
+    name: 'Improvements'
+  },
+  {
+    id: 26,
+    expensesId: 2,
+    name: 'Other Home Expenses'
+  },
+  {
+    id: 27,
+    expensesId: 3,
+    name: 'Electricity'
+  },
+  {
+    id: 28,
+    expensesId: 3,
+    name: 'Gas/Oil'
+  },
+  {
+    id: 29,
+    expensesId: 3,
+    name: 'Water/Sewer'
+  },
+  {
+    id: 30,
+    expensesId: 3,
+    name: 'Trash'
+  },
+  {
+    id: 31,
+    expensesId: 3,
+    name: 'Phone'
+  },
+  {
+    id: 32,
+    expensesId: 3,
+    name: 'Cable/Satellite'
+  },
+  {
+    id: 33,
+    expensesId: 3,
+    name: 'Internet'
+  },
+  {
+    id: 34,
+    expensesId: 3,
+    name: 'Other Utilities'
+  },
+  {
+    id: 35,
+    expensesId: 4,
+    name: 'Groceries'
+  },
+  {
+    id: 36,
+    expensesId: 4,
+    name: 'Dining / Eating Out'
+  },
+  {
+    id: 37,
+    expensesId: 4,
+    name: 'Pet Food'
+  },
+  {
+    id: 38,
+    expensesId: 4,
+    name: 'Other Food'
+  },
+  {
+    id: 39,
+    expensesId: 5,
+    name: 'Vehicle Payments'
+  },
+  {
+    id: 40,
+    expensesId: 5,
+    name: 'Auto Insurance'
+  },
+  {
+    id: 41,
+    expensesId: 5,
+    name: 'Fuel'
+  },
+  {
+    id: 42,
+    expensesId: 5,
+    name: 'Bus/Taxi/Train Fare'
+  },
+  {
+    id: 43,
+    expensesId: 5,
+    name: 'Repairs/Tires'
+  },
+  {
+    id: 44,
+    expensesId: 5,
+    name: 'Registration/License'
+  },
+  {
+    id: 45,
+    expensesId: 5,
+    name: 'Other Transportation'
+  },
+  {
+    id: 46,
+    expensesId: 6,
+    name: 'Health Insurance'
+  },
+  {
+    id: 47,
+    expensesId: 6,
+    name: 'Disability Insurance'
+  },
+  {
+    id: 48,
+    expensesId: 6,
+    name: 'Doctor/Dentist/Optometrist'
+  },
+  {
+    id: 49,
+    expensesId: 6,
+    name: 'Medicine/Drugs'
+  },
+  {
+    id: 50,
+    expensesId: 6,
+    name: 'Health Club Dues'
+  },
+  {
+    id: 51,
+    expensesId: 6,
+    name: 'Life Insurance'
+  },
+  {
+    id: 52,
+    expensesId: 6,
+    name: 'Veterinarian/Pet Care'
+  },
+  {
+    id: 53,
+    expensesId: 6,
+    name: 'Other Health'
+  },
+]
